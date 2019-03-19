6579316c3a0e57613524189b78162b4fac376d6e
Arthur Moraux <armoraux@len-009.oma.be>
2019-03-19 10:50:25 +0100

    1st setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='logger',
+    version='0.0.1',
+    author='Arthur Moraux',
+    author_email='arthur.moraux@gmail.com',
+    description='A logger utility for tracking metrics values during training of pyTorch models.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ArthMx/Logger',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
